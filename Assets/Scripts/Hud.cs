108c841874d84e2f1d58ba49be5fbc97d2a8a7be
Jake Haas <haasjac@gmail.com>
2016-01-16 19:55:01 -0500

    Jake Initial
    I should have committed a long time ago

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+
+    public Text rupee_text;
+    public Text key_text;
+    public Text bomb_text;
+    public Text health_text;
+    private string health;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rupee_text.text = "Rupees: " + PlayerControl.S.rupee_count.ToString();
+        key_text.text = "Keys: " + PlayerControl.S.key_count.ToString();
+        bomb_text.text = "Bombs: " + PlayerControl.S.bomb_count.ToString();
+        health = "";
+        for (int i =0; i < Mathf.FloorToInt(PlayerControl.S.health); i++) {
+            health += "<3";
+        }
+        if (PlayerControl.S.health - Mathf.FloorToInt(PlayerControl.S.health) != 0) {
+            health += "<";
+        }
+        health_text.text = health;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction {NORTH, EAST, SOUTH, WEST};
+public enum EntityState {NORMAL, ATTACKING};
+
+public class PlayerControl : MonoBehaviour {
+    
+    public static PlayerControl S;
+
+    public float walking_veloctiy = 1.0f;
+
+    public int rupee_count = 0;
+    public int key_count = 0;
+    public int bomb_count = 0;
+    public float health = 3.0f;
+
+    public bool invincible = false;
+
+    public Sprite[] link_run_down;
+	public Sprite[] link_run_up;
+	public Sprite[] link_run_right;
+	public Sprite[] link_run_left;
+
+	StateMachine animation_state_machine;
+	StateMachine control_state_machine;
+	
+	public EntityState current_state = EntityState.NORMAL;
+	public Direction current_direction = Direction.SOUTH;
+
+	public GameObject selected_weapon_prefab;
+
+	// Use this for initialization
+	void Start () {
+        Application.targetFrameRate = 60;
+
+        if(S != null) {
+            Debug.LogError("WTF TWO LINKS???");
+        }
+        S = this;
+
+        animation_state_machine = new StateMachine();
+        animation_state_machine.ChangeState(new StateIdleWithSprite(this, GetComponent<SpriteRenderer>(), link_run_down[0]));
+        control_state_machine = new StateMachine();
+        control_state_machine.ChangeState(new StateLinkNormalMovement(this));
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (health <= 0)
+            Destroy(this.gameObject);
+
+        animation_state_machine.Update();
+
+        control_state_machine.Update();
+        if(control_state_machine.IsFinished()) {
+            control_state_machine.ChangeState(new StateLinkNormalMovement(this));
+        }
+    }
+
+    void OnTriggerEnter(Collider coll) {
+
+        switch (coll.gameObject.tag) {
+            case "Rupee":
+                rupee_count++;
+                Destroy(coll.gameObject);
+                break;
+            case "Heart":
+                health++;
+                Destroy(coll.gameObject);
+                break;
+            case "Bomb":
+                bomb_count++;
+                Destroy(coll.gameObject);
+                break;
+            case "Key":
+                key_count++;
+                Destroy(coll.gameObject);
+                break;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll) {
+
+        switch (coll.gameObject.tag) {
+            case "Enemy":
+                control_state_machine.ChangeState(new StateDamaged(this, coll.contacts[0].normal, 0.5f));
+                break;
+            case "EnemyProjectile":
+                control_state_machine.ChangeState(new StateDamaged(this, coll.contacts[0].normal, 0.5f));
+                break;
+        }
+    }
+
+    public void pcInvoke(string func, float time) {
+        Invoke(func, time);
+    }
+
+    public void makeVulnerable() {
+        invincible = false;
+    }
+}
