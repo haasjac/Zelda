82b9aca57659dbd375489656e71fe6b4fce1cd9b
Jake Haas <haasjac@gmail.com>
2016-01-20 12:46:45 -0500

    Item alive timer

---
+++ Assets/item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item : MonoBehaviour {
+
+    public float time_alive = 4f;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= time_alive) {
+            Destroy(this.gameObject);
+        }
+	}
+}
