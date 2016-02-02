59d7c9aebae3621051c9a3b2491c7d20ed664eee
Jake Haas <haasjac@gmail.com>
2016-02-02 00:21:36 -0500

    Main Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,26 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
+    public Image sword;
+    int index;
+
 	// Use this for initialization
 	void Start () {
-	
+        index = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-        if(Input.GetKeyDown(KeyCode.O)) {
-            Application.LoadLevel("Dungeon");
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetButtonDown("Vertical")) {
+            index++;
         }
 
-        if (Input.GetKeyDown(KeyCode.P)) {
-            Application.LoadLevel("Custom_Level");
+        if(Input.GetButtonDown("Select")) {
+            index++;
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            Application.LoadLevel("Main_Menu");
+        Vector3 pos = sword.GetComponent<RectTransform>().anchoredPosition;
+
+        if (index % 2 == 0) {
+            pos.y = 23.5f;
+        } else if (index % 2 == 1) {
+            pos.y = 7.5f;
+        }
+
+        sword.GetComponent<RectTransform>().anchoredPosition = pos;
+
+        if (Input.GetButtonDown("Start")) {
+            if (index % 2 == 0) {
+                Application.LoadLevel("Dungeon");
+            } else {
+                Application.LoadLevel("Custom_Level");
+            }
         }
     }
 }
